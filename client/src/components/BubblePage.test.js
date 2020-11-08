df4dcf4b98904bc45d97f179a22828e3fa55b006
Andrew Ruiz <nukedoom@gmail.com>
2020-11-08 12:22:24 -0800

    Completed Sprint

--- client/src/components/BubblePage.test.js
+++ client/src/components/BubblePage.test.js
@@ -1,7 +1,37 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import BubblePage from "./BubblePage";
+import ColorList from "./ColorList";
+
+const testColors = [
+  {
+    color: "aliceblue",
+    code: {
+      hex: "#f0f8ff"
+    },
+    id: 1
+  },
+  {
+    color: "limegreen",
+    code: {
+      hex: "#99ddbc"
+    },
+    id: 2
+  }
+]
 
 test("Fetches data and renders the bubbles", () => {
   // Finish this test
+
+  // Seeing if page even loads
+  render(<BubblePage />)
+
+  // Loading page again with an array of colors
+  const { rerender } = render(<ColorList colors={testColors} />)
+
+  const screenTest = screen.getAllByTestId(/colors/i)
+
+  // Checking to see that our sample arry has loaded the information
+  expect(screenTest).toHaveLength(2)
+
 });
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,69 @@
-import React from "react";
+import React, { useState }  from "react";
+import axios from 'axios'
+import { axiosWithAuth } from '../utils/axiosWithAuth'
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const { push } = useHistory()
+
+  const [newUser, setNewUser] = useState({
+    credentials: {
+      username: "",
+      password: "",
+    }
+  });
+
+  const handleChange = (e) => {
+    setNewUser({
+      credentials: {
+        ...newUser.credentials,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  const login = (e) => {
+    e.preventDefault()
+    axiosWithAuth()
+      .post('http://localhost:5000/api/login',newUser.credentials)
+      .then(res => {
+        console.log(res)
+        localStorage.setItem('token', res.data.payload)
+
+        push('/protected')
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+
+      <form onSubmit={login}>
+
+        <input
+          type="text"
+          name="username"
+          value={newUser.username}
+          onChange={handleChange}
+        >
+        </input>
+
+        <input
+          type="password"
+          name="password"
+          value={newUser.password}
+          onChange={handleChange}
+        >
+        </input>
+
+        <button>Log In</button>
+
+      </form>
     </>
   );
 };
